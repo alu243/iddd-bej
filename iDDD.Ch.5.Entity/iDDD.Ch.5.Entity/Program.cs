4eeb797b0fcc41d5a1d6fd2de88f18b75d9f257a
alu243 <alu243@gmail.com>
2019-06-04 23:58:58 +0800

    Initial Project & modify .gitignore

---
+++ iDDD.Ch.5.Entity/iDDD.Ch.5.Entity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace iDDD.Ch._5.Entity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
